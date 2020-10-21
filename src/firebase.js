4fdf485f2e63c420ad8b161623bf2cce00db6fa7
lawrencelucky <lawrencelucky1999@gmail.com>
2020-10-21 10:28:03 +0100

    Feat: Initialising the project

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+let firebaseConfig = {
+  apiKey: 'AIzaSyCylzzR5kQDgYep-C8KTkP3GsJcNzbwbuM',
+  authDomain: 'clone-881ba.firebaseapp.com',
+  databaseURL: 'https://clone-881ba.firebaseio.com',
+  projectId: 'clone-881ba',
+  storageBucket: 'clone-881ba.appspot.com',
+  messagingSenderId: '160035394103',
+  appId: '1:160035394103:web:88ecefaf058cc1878fe5b2',
+  measurementId: 'G-T0MVWHCMCP',
+};
